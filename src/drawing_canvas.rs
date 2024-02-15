samitbasu/surfer-project-rhdl

Add inlined util functions for more readable code

src/drawing_canvas.rs
@@ -462,7 +462,7 @@ impl State {
                 [first, second, ..] => second - first > 15.,
             };
             let ticks = &draw_data.ticks;
-            if !ticks.is_empty() && self.show_ticks.unwrap_or(self.config.layout.show_ticks()) {
+            if !ticks.is_empty() && self.get_show_ticks() {
                 let stroke = Stroke {
                     color: self.config.ticks.style.color,
                     width: self.config.ticks.style.width,

src/menus.rs
@@ -187,37 +187,26 @@ impl State {
                 }
             });
 
-            ui.radio(
-                self.show_ticks.unwrap_or(self.config.layout.show_ticks()),
-                "Show tick lines",
-            )
-            .clicked()
-            .then(|| {
-                ui.close_menu();
-                msgs.push(Message::ToggleTickLines)
-            });
+            ui.radio(self.get_show_ticks(), "Show tick lines")
+                .clicked()
+                .then(|| {
+                    ui.close_menu();
+                    msgs.push(Message::ToggleTickLines)
+                });
 
-            ui.radio(
-                self.show_variable_tooltip
-                    .unwrap_or(self.config.layout.show_tooltip()),
-                "Show variable tooltip",
-            )
-            .clicked()
-            .then(|| {
-                ui.close_menu();
-                msgs.push(Message::ToggleVariableTooltip)
-            });
+            ui.radio(self.get_show_tooltip(), "Show variable tooltip")
+                .clicked()
+                .then(|| {
+                    ui.close_menu();
+                    msgs.push(Message::ToggleVariableTooltip)
+                });
 
-            ui.radio(
-                self.show_variable_indices
-                    .unwrap_or(self.config.layout.show_variable_indices()),
-                "Show variable indices",
-            )
-            .clicked()
-            .then(|| {
-                ui.close_menu();
-                msgs.push(Message::ToggleIndices)
-            });
+            ui.radio(self.get_show_variable_indices(), "Show variable indices")
+                .clicked()
+                .then(|| {
+                    ui.close_menu();
+                    msgs.push(Message::ToggleIndices)
+                });
         });
         ui.menu_button("Help", |ui| {
             b("Quick start", Message::SetQuickStartVisible(true)).add_closing_menu(msgs, ui);

src/state_util.rs
@@ -1,0 +1,62 @@
+// Utility functions, typically inlined, for more readable code
+
+use eframe::epaint::Color32;
+
+use crate::{displayed_item::DisplayedItem, State};
+
+impl State {
+    #[inline]
+    pub fn get_item_text_color(&self, item: &DisplayedItem) -> &Color32 {
+        item.color()
+            .and_then(|color| self.config.theme.colors.get(&color))
+            .unwrap_or(&self.config.theme.foreground)
+    }
+
+    #[inline]
+    pub fn get_show_statusbar(&self) -> bool {
+        self.show_statusbar
+            .unwrap_or_else(|| self.config.layout.show_statusbar())
+    }
+
+    #[inline]
+    pub fn get_show_toolbar(&self) -> bool {
+        self.show_toolbar
+            .unwrap_or_else(|| self.config.layout.show_toolbar())
+    }
+
+    #[inline]
+    pub fn get_show_overview(&self) -> bool {
+        self.show_overview
+            .unwrap_or_else(|| self.config.layout.show_overview())
+    }
+
+    #[inline]
+    pub fn get_show_hierarchy(&self) -> bool {
+        self.show_hierarchy
+            .unwrap_or_else(|| self.config.layout.show_hierarchy())
+    }
+
+    #[inline]
+    pub fn get_show_tooltip(&self) -> bool {
+        self.show_tooltip
+            .unwrap_or_else(|| self.config.layout.show_tooltip())
+    }
+
+    #[inline]
+    pub fn get_show_ticks(&self) -> bool {
+        self.show_ticks
+            .unwrap_or_else(|| self.config.layout.show_ticks())
+    }
+
+    #[inline]
+    pub fn get_show_menu(&self) -> bool {
+        self.show_menu
+            .unwrap_or_else(|| self.config.layout.show_menu())
+    }
+
+    #[inline]
+    pub fn get_show_variable_indices(&self) -> bool {
+        self.show_variable_indices
+            .unwrap_or_else(|| self.config.layout.show_variable_indices())
+    }
+}

binhkieudo/surfer

src/drawing_canvas.rs
@@ -427,7 +427,7 @@ impl State {
                 [first, second, ..] => second - first > 15.,
             };
             let ticks = &draw_data.ticks;
-            if !ticks.is_empty() && self.show_ticks.unwrap_or(self.config.layout.show_ticks()) {
+            if !ticks.is_empty() && self.show_ticks() {
                 let stroke = Stroke {
                     color: self.config.ticks.style.color,
                     width: self.config.ticks.style.width,

src/menus.rs
@@ -187,37 +187,26 @@ impl State {
                 }
             });
 
-            ui.radio(
-                self.show_ticks.unwrap_or(self.config.layout.show_ticks()),
-                "Show tick lines",
-            )
-            .clicked()
-            .then(|| {
-                ui.close_menu();
-                msgs.push(Message::ToggleTickLines)
-            });
+            ui.radio(self.show_ticks(), "Show tick lines")
+                .clicked()
+                .then(|| {
+                    ui.close_menu();
+                    msgs.push(Message::ToggleTickLines)
+                });
 
-            ui.radio(
-                self.show_variable_tooltip
-                    .unwrap_or(self.config.layout.show_tooltip()),
-                "Show variable tooltip",
-            )
-            .clicked()
-            .then(|| {
-                ui.close_menu();
-                msgs.push(Message::ToggleVariableTooltip)
-            });
+            ui.radio(self.show_tooltip(), "Show variable tooltip")
+                .clicked()
+                .then(|| {
+                    ui.close_menu();
+                    msgs.push(Message::ToggleVariableTooltip)
+                });
 
-            ui.radio(
-                self.show_variable_indices
-                    .unwrap_or(self.config.layout.show_variable_indices()),
-                "Show variable indices",
-            )
-            .clicked()
-            .then(|| {
-                ui.close_menu();
-                msgs.push(Message::ToggleIndices)
-            });
+            ui.radio(self.show_variable_indices(), "Show variable indices")
+                .clicked()
+                .then(|| {
+                    ui.close_menu();
+                    msgs.push(Message::ToggleIndices)
+                });
         });
         ui.menu_button("Help", |ui| {
             b("Quick start", Message::SetQuickStartVisible(true)).add_closing_menu(msgs, ui);

src/state_util.rs
@@ -1,0 +1,62 @@
+// Utility functions, typically inlined, for more readable code
+
+use eframe::epaint::Color32;
+
+use crate::{displayed_item::DisplayedItem, State};
+
+impl State {
+    #[inline]
+    pub fn get_item_text_color(&self, item: &DisplayedItem) -> &Color32 {
+        item.color()
+            .and_then(|color| self.config.theme.colors.get(&color))
+            .unwrap_or(&self.config.theme.foreground)
+    }
+
+    #[inline]
+    pub fn show_statusbar(&self) -> bool {
+        self.show_statusbar
+            .unwrap_or_else(|| self.config.layout.show_statusbar())
+    }
+
+    #[inline]
+    pub fn show_toolbar(&self) -> bool {
+        self.show_toolbar
+            .unwrap_or_else(|| self.config.layout.show_toolbar())
+    }
+
+    #[inline]
+    pub fn show_overview(&self) -> bool {
+        self.show_overview
+            .unwrap_or_else(|| self.config.layout.show_overview())
+    }
+
+    #[inline]
+    pub fn show_hierarchy(&self) -> bool {
+        self.show_hierarchy
+            .unwrap_or_else(|| self.config.layout.show_hierarchy())
+    }
+
+    #[inline]
+    pub fn show_tooltip(&self) -> bool {
+        self.show_tooltip
+            .unwrap_or_else(|| self.config.layout.show_tooltip())
+    }
+
+    #[inline]
+    pub fn show_ticks(&self) -> bool {
+        self.show_ticks
+            .unwrap_or_else(|| self.config.layout.show_ticks())
+    }
+
+    #[inline]
+    pub fn show_menu(&self) -> bool {
+        self.show_menu
+            .unwrap_or_else(|| self.config.layout.show_menu())
+    }
+
+    #[inline]
+    pub fn show_variable_indices(&self) -> bool {
+        self.show_variable_indices
+            .unwrap_or_else(|| self.config.layout.show_variable_indices())
+    }
+}