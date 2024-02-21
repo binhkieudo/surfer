samitbasu/surfer-project-rhdl

Support showing multiple viewports

src/keys.rs
@@ -74,39 +74,47 @@ impl State {
                     (Key::M, true, false, false) => msgs.push(Message::ToggleMenu),
                     (Key::T, true, false, false) => msgs.push(Message::ToggleToolbar),
                     (Key::F11, true, false, _) => msgs.push(Message::ToggleFullscreen),
-                    (Key::S, true, false, false) => msgs.push(Message::GoToStart),
-                    (Key::E, true, false, false) => msgs.push(Message::GoToEnd),
+                    (Key::S, true, false, false) => {
+                        msgs.push(Message::GoToStart { viewport_idx: 0 })
+                    }
+                    (Key::E, true, false, false) => msgs.push(Message::GoToEnd { viewport_idx: 0 }),
                     (Key::Minus, true, false, false) => msgs.push(Message::CanvasZoom {
                         mouse_ptr_timestamp: None,
                         delta: 2.0,
+                        viewport_idx: 0,
                     }),
                     (Key::PlusEquals, true, false, false) => msgs.push(Message::CanvasZoom {
                         mouse_ptr_timestamp: None,
                         delta: 0.5,
+                        viewport_idx: 0,
                     }),
                     (Key::PageUp, true, false, false) => msgs.push(Message::CanvasScroll {
                         delta: Vec2 {
                             x: 0.,
                             y: -PER_SCROLL_EVENT * SCROLL_EVENTS_PER_PAGE,
                         },
+                        viewport_idx: 0,
                     }),
                     (Key::PageDown, true, false, false) => msgs.push(Message::CanvasScroll {
                         delta: Vec2 {
                             x: 0.,
                             y: PER_SCROLL_EVENT * SCROLL_EVENTS_PER_PAGE,
                         },
+                        viewport_idx: 0,
                     }),
                     (Key::ArrowRight, true, false, false) => msgs.push(Message::CanvasScroll {
                         delta: Vec2 {
                             x: 0.,
                             y: -PER_SCROLL_EVENT,
                         },
+                        viewport_idx: 0,
                     }),
                     (Key::ArrowLeft, true, false, false) => msgs.push(Message::CanvasScroll {
                         delta: Vec2 {
                             x: 0.,
                             y: PER_SCROLL_EVENT,
                         },
+                        viewport_idx: 0,
                     }),
                     (Key::J, true, false, false) => {
                         if modifiers.alt {
@@ -190,6 +198,6 @@ fn handle_digit(digit: u8, modifiers: &egui::Modifiers, msgs: &mut Vec<Message>)
     } else if modifiers.ctrl {
         msgs.push(Message::SetCursorPosition(digit))
     } else {
-        msgs.push(Message::GoToCursorPosition(digit))
+        msgs.push(Message::GoToCursorPosition(digit, 0))
     }
 }

src/main.rs
@@ -692,42 +692,48 @@ impl State {
                     }
                 }
             }
-            Message::CanvasScroll { delta } => {
+            Message::CanvasScroll {
+                delta,
+                viewport_idx,
+            } => {
                 if let Some(waves) = self.waves.as_mut() {
-                    waves.handle_canvas_scroll(delta);
+                    waves.viewports[viewport_idx]
+                        .handle_canvas_scroll(delta.y as f64 + delta.x as f64);
                     self.invalidate_draw_commands();
                 }
             }
             Message::CanvasZoom {
                 delta,
                 mouse_ptr_timestamp,
+                viewport_idx,
             } => {
                 if let Some(waves) = self.waves.as_mut() {
-                    waves.handle_canvas_zoom(mouse_ptr_timestamp, delta as f64);
+                    waves.viewports[viewport_idx]
+                        .handle_canvas_zoom(mouse_ptr_timestamp, delta as f64);
                     self.invalidate_draw_commands();
                 }
             }
-            Message::ZoomToFit => {
+            Message::ZoomToFit { viewport_idx } => {
                 if let Some(waves) = &mut self.waves {
-                    waves.zoom_to_fit();
+                    waves.viewports[viewport_idx].zoom_to_fit(&waves.num_timestamps);
                     self.invalidate_draw_commands();
                 }
             }
-            Message::GoToEnd => {
+            Message::GoToEnd { viewport_idx } => {
                 if let Some(waves) = &mut self.waves {
-                    waves.go_to_end();
+                    waves.viewports[viewport_idx].go_to_end(&waves.num_timestamps);
                     self.invalidate_draw_commands();
                 }
             }
-            Message::GoToStart => {
+            Message::GoToStart { viewport_idx } => {
                 if let Some(waves) = &mut self.waves {
-                    waves.go_to_start();
+                    waves.viewports[viewport_idx].go_to_start();
                     self.invalidate_draw_commands();
                 }
             }
-            Message::GoToTime(time) => {
+            Message::GoToTime(time, viewport_idx) => {
                 if let Some(waves) = self.waves.as_mut() {
-                    waves.go_to_time(&time.clone());
+                    waves.viewports[viewport_idx].go_to_time(&time.clone());
                     self.invalidate_draw_commands();
                 };
             }
@@ -735,10 +741,13 @@ impl State {
                 self.invalidate_draw_commands();
                 self.wanted_timeunit = timeunit;
             }
-            Message::ZoomToRange { start, end } => {
+            Message::ZoomToRange {
+                start,
+                end,
+                viewport_idx,
+            } => {
                 if let Some(waves) = &mut self.waves {
-                    waves.viewport.curr_left = start;
-                    waves.viewport.curr_right = end;
+                    waves.viewports[viewport_idx].zoom_to_range(start, end);
                     self.invalidate_draw_commands();
                 }
             }
@@ -839,21 +848,23 @@ impl State {
                     .map(|t| t.to_bigint().unwrap())
                     .unwrap_or(BigInt::from_u32(1).unwrap());
                 let viewport = Viewport::new(0., num_timestamps.clone().to_f64().unwrap());
+                let viewports = [viewport].to_vec();
 
                 let new_wave = if keep_signals && self.waves.is_some() {
                     self.waves.take().unwrap().update_with(
                         new_waves,
                         filename,
                         num_timestamps,
-                        viewport,
+                        viewports.repeat(self.waves.as_ref().unwrap().viewports.len()),
                         &self.sys.translators,
                     )
                 } else if let Some(old) = self.previous_waves.take() {
+                    let viewport_count = old.viewports.len();
                     old.update_with(
                         new_waves,
                         filename,
                         num_timestamps,
-                        viewport,
+                        viewports.repeat(viewport_count),
                         &self.sys.translators,
                     )
                 } else {
@@ -862,7 +873,7 @@ impl State {
                         source: filename,
                         active_module: None,
                         displayed_items: vec![],
-                        viewport,
+                        viewports,
                         signal_format: HashMap::new(),
                         num_timestamps,
                         cursor: None,
@@ -984,10 +995,10 @@ impl State {
                     waves.set_cursor_position(idx);
                 };
             }
-            Message::GoToCursorPosition(idx) => {
+            Message::GoToCursorPosition(idx, viewport_idx) => {
                 if let Some(waves) = self.waves.as_mut() {
                     if let Some(cursor) = waves.cursors.get(&idx) {
-                        waves.go_to_time(&cursor.clone());
+                        waves.viewports[viewport_idx].go_to_time(&cursor);
                         self.invalidate_draw_commands();
                     }
                 };
@@ -1082,6 +1093,20 @@ impl State {
                 );
             }
             Message::Exit | Message::ToggleFullscreen => {} // Handled in eframe::update
+            Message::AddViewport => {
+                if let Some(waves) = &mut self.waves {
+                    let viewport =
+                        Viewport::new(0., waves.num_timestamps.clone().to_f64().unwrap());
+                    waves.viewports.push(viewport)
+                }
+            }
+            Message::RemoveViewport => {
+                if let Some(waves) = &mut self.waves {
+                    if waves.viewports.len() > 1 {
+                        waves.viewports.pop();
+                    }
+                }
+            }
         }
     }
 

src/tests/snapshot.rs
@@ -504,27 +504,27 @@ snapshot_ui_with_file_and_msgs! {timeline_render, "examples/counter.vcd", [
 
 snapshot_ui_with_file_and_msgs! {negative_cursorlocation, "examples/counter.vcd", [
     Message::AddModule(ModuleRef::from_strs(&["tb"])),
-    Message::GoToTime(BigInt::from(-50)),
+    Message::GoToTime(BigInt::from(-50), 0),
     Message::CursorSet(BigInt::from(-100)),
 ]}
 
 snapshot_ui_with_file_and_msgs! {goto_start, "examples/counter.vcd", [
     Message::AddModule(ModuleRef::from_strs(&["tb"])),
-    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2},
-    Message::GoToStart
+    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2, viewport_idx: 0},
+    Message::GoToStart {viewport_idx: 0}
 ]}
 
 snapshot_ui_with_file_and_msgs! {goto_end, "examples/counter.vcd", [
     Message::AddModule(ModuleRef::from_strs(&["tb"])),
-    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2},
-    Message::GoToEnd
+    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2, viewport_idx: 0},
+    Message::GoToEnd {viewport_idx: 0}
 ]}
 
 snapshot_ui_with_file_and_msgs! {zoom_to_fit, "examples/counter.vcd", [
     Message::AddModule(ModuleRef::from_strs(&["tb"])),
-    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2},
-    Message::GoToEnd,
-    Message::ZoomToFit
+    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2, viewport_idx: 0},
+    Message::GoToEnd {viewport_idx: 0},
+    Message::ZoomToFit {viewport_idx: 0}
 ]}
 
 snapshot_ui!(regex_error_indication, || {
@@ -700,3 +700,11 @@ snapshot_ui!(start_signal_filter_works, || {
     state.sys.signal_filter.borrow_mut().push_str("a");
     state
 });
+
+snapshot_ui_with_file_and_msgs! {add_viewport_works, "examples/counter.vcd", [
+    Message::AddViewport, Message::AddViewport, Message::SetActiveScope(ModuleRef::from_strs(&["tb"])), Message::AddSignal(SignalRef::from_hierarchy_string("tb.clk")), Message::AddTimeLine(None),
+]}
+
+snapshot_ui_with_file_and_msgs! {remove_viewport_works, "examples/counter.vcd", [
+    Message::AddViewport, Message::AddViewport, Message::SetActiveScope(ModuleRef::from_strs(&["tb"])), Message::AddSignal(SignalRef::from_hierarchy_string("tb.clk")), Message::AddTimeLine(None), Message::RemoveViewport
+]}

binhkieudo/surfer

src/keys.rs
@@ -76,19 +76,23 @@ impl State {
                     (Key::M, true, false, false) => msgs.push(Message::ToggleMenu),
                     (Key::T, true, false, false) => msgs.push(Message::ToggleToolbar),
                     (Key::F11, true, false, _) => msgs.push(Message::ToggleFullscreen),
-                    (Key::S, true, false, false) => msgs.push(Message::GoToStart),
-                    (Key::E, true, false, false) => msgs.push(Message::GoToEnd),
+                    (Key::S, true, false, false) => {
+                        msgs.push(Message::GoToStart { viewport_idx: 0 })
+                    }
+                    (Key::E, true, false, false) => msgs.push(Message::GoToEnd { viewport_idx: 0 }),
                     (Key::R, true, false, false) => msgs.push(Message::ReloadWaveform(
                         self.config.behavior.keep_during_reload,
                     )),
                     (Key::Minus, true, false, false) => msgs.push(Message::CanvasZoom {
                         mouse_ptr_timestamp: None,
                         delta: 2.0,
+                        viewport_idx: 0,
                     }),
                     (Key::Plus | Key::Equals, true, false, false) => {
                         msgs.push(Message::CanvasZoom {
                             mouse_ptr_timestamp: None,
                             delta: 0.5,
+                            viewport_idx: 0,
                         })
                     }
                     (Key::PageUp, true, false, false) => msgs.push(Message::CanvasScroll {
@@ -96,24 +100,28 @@ impl State {
                             x: 0.,
                             y: -PER_SCROLL_EVENT * SCROLL_EVENTS_PER_PAGE,
                         },
+                        viewport_idx: 0,
                     }),
                     (Key::PageDown, true, false, false) => msgs.push(Message::CanvasScroll {
                         delta: Vec2 {
                             x: 0.,
                             y: PER_SCROLL_EVENT * SCROLL_EVENTS_PER_PAGE,
                         },
+                        viewport_idx: 0,
                     }),
                     (Key::ArrowRight, true, false, false) => msgs.push(Message::CanvasScroll {
                         delta: Vec2 {
                             x: 0.,
                             y: -PER_SCROLL_EVENT,
                         },
+                        viewport_idx: 0,
                     }),
                     (Key::ArrowLeft, true, false, false) => msgs.push(Message::CanvasScroll {
                         delta: Vec2 {
                             x: 0.,
                             y: PER_SCROLL_EVENT,
                         },
+                        viewport_idx: 0,
                     }),
                     (Key::J, true, false, false) => {
                         if modifiers.alt {
@@ -197,6 +205,6 @@ fn handle_digit(digit: u8, modifiers: &Modifiers, msgs: &mut Vec<Message>) {
     } else if modifiers.ctrl {
         msgs.push(Message::MoveMarkerToCursor(digit))
     } else {
-        msgs.push(Message::GoToMarkerPosition(digit))
+        msgs.push(Message::GoToMarkerPosition(digit, 0))
     }
 }

src/main.rs
@@ -377,7 +377,7 @@ pub struct SystemState {
     /// The draw commands for every variable currently selected
     // For performance reasons, these need caching so we have them in a RefCell for interior
     // mutability
-    draw_data: RefCell<Option<CachedDrawData>>,
+    draw_data: RefCell<Vec<Option<CachedDrawData>>>,
 
     gesture_start_location: Option<emath::Pos2>,
 
@@ -419,7 +419,7 @@ impl SystemState {
             startup_commands: vec![],
             url: RefCell::new(String::new()),
             command_prompt_text: RefCell::new(String::new()),
-            draw_data: RefCell::new(None),
+            draw_data: RefCell::new(vec![None]),
             last_canvas_rect: RefCell::new(None),
             variable_name_filter: RefCell::new(String::new()),
             item_renaming_string: RefCell::new(String::new()),
@@ -747,43 +747,49 @@ impl State {
                     }
                 }
             }
-            Message::CanvasScroll { delta } => {
+            Message::CanvasScroll {
+                delta,
+                viewport_idx,
+            } => {
                 if let Some(waves) = self.waves.as_mut() {
-                    waves.handle_canvas_scroll(delta);
+                    waves.viewports[viewport_idx]
+                        .handle_canvas_scroll(delta.y as f64 + delta.x as f64);
                     self.invalidate_draw_commands();
                 }
             }
             Message::CanvasZoom {
                 delta,
                 mouse_ptr_timestamp,
+                viewport_idx,
             } => {
                 if let Some(waves) = self.waves.as_mut() {
-                    waves.handle_canvas_zoom(mouse_ptr_timestamp, delta as f64);
+                    waves.viewports[viewport_idx]
+                        .handle_canvas_zoom(mouse_ptr_timestamp, delta as f64);
                     self.invalidate_draw_commands();
                 }
             }
-            Message::ZoomToFit => {
+            Message::ZoomToFit { viewport_idx } => {
                 if let Some(waves) = &mut self.waves {
-                    waves.zoom_to_fit();
+                    waves.viewports[viewport_idx].zoom_to_fit(&waves.num_timestamps);
                     self.invalidate_draw_commands();
                 }
             }
-            Message::GoToEnd => {
+            Message::GoToEnd { viewport_idx } => {
                 if let Some(waves) = &mut self.waves {
-                    waves.go_to_end();
+                    waves.viewports[viewport_idx].go_to_end(&waves.num_timestamps);
                     self.invalidate_draw_commands();
                 }
             }
-            Message::GoToStart => {
+            Message::GoToStart { viewport_idx } => {
                 if let Some(waves) = &mut self.waves {
-                    waves.go_to_start();
+                    waves.viewports[viewport_idx].go_to_start();
                     self.invalidate_draw_commands();
                 }
             }
-            Message::GoToTime(time) => {
+            Message::GoToTime(time, viewport_idx) => {
                 if let Some(waves) = self.waves.as_mut() {
                     if let Some(time) = time {
-                        waves.go_to_time(&time);
+                        waves.viewports[viewport_idx].go_to_time(&time.clone());
                         self.invalidate_draw_commands();
                     }
                 };
@@ -796,10 +802,13 @@ impl State {
                 self.time_string_format = format;
                 self.invalidate_draw_commands();
             }
-            Message::ZoomToRange { start, end } => {
+            Message::ZoomToRange {
+                start,
+                end,
+                viewport_idx,
+            } => {
                 if let Some(waves) = &mut self.waves {
-                    waves.viewport.curr_left = start;
-                    waves.viewport.curr_right = end;
+                    waves.viewports[viewport_idx].zoom_to_range(start, end);
                     self.invalidate_draw_commands();
                 }
             }
@@ -906,24 +915,27 @@ impl State {
                     .map(|t| t.to_bigint().unwrap())
                     .unwrap_or_else(|| BigInt::from_u32(1).unwrap());
                 let viewport = Viewport::new(0., num_timestamps.clone().to_f64().unwrap());
+                let viewports = [viewport].to_vec();
 
                 let new_wave = if load_options.keep_variables && self.waves.is_some() {
+                    let old_viewport_count = self.waves.as_ref().unwrap().viewports.len();
                     self.waves.take().unwrap().update_with(
                         new_waves,
                         filename,
                         format,
                         num_timestamps,
-                        viewport,
+                        viewports.repeat(old_viewport_count),
                         &self.sys.translators,
                         load_options.keep_unavailable,
                     )
                 } else if let Some(old) = self.previous_waves.take() {
+                    let viewport_count = old.viewports.len();
                     old.update_with(
                         new_waves,
                         filename,
                         format,
                         num_timestamps,
-                        viewport,
+                        viewports.repeat(viewport_count),
                         &self.sys.translators,
                         load_options.keep_unavailable,
                     )
@@ -934,7 +946,7 @@ impl State {
                         format,
                         active_scope: None,
                         displayed_items: vec![],
-                        viewport,
+                        viewports,
                         variable_format: HashMap::new(),
                         num_timestamps,
                         cursor: None,
@@ -1114,10 +1126,10 @@ impl State {
                     waves.move_marker_to_cursor(idx);
                 };
             }
-            Message::GoToMarkerPosition(idx) => {
+            Message::GoToMarkerPosition(idx, viewport_idx) => {
                 if let Some(waves) = self.waves.as_mut() {
                     if let Some(cursor) = waves.markers.get(&idx) {
-                        waves.go_to_time(&cursor.clone());
+                        waves.viewports[viewport_idx].go_to_time(&cursor);
                         self.invalidate_draw_commands();
                     }
                 };
@@ -1218,6 +1230,22 @@ impl State {
                 }
             }
             Message::Exit | Message::ToggleFullscreen => {} // Handled in eframe::update
+            Message::AddViewport => {
+                if let Some(waves) = &mut self.waves {
+                    let viewport =
+                        Viewport::new(0., waves.num_timestamps.clone().to_f64().unwrap());
+                    waves.viewports.push(viewport);
+                    self.sys.draw_data.borrow_mut().push(None);
+                }
+            }
+            Message::RemoveViewport => {
+                if let Some(waves) = &mut self.waves {
+                    if waves.viewports.len() > 1 {
+                        waves.viewports.pop();
+                        self.sys.draw_data.borrow_mut().pop();
+                    }
+                }
+            }
         }
     }
 

src/tests/snapshot.rs
@@ -584,7 +584,7 @@ snapshot_ui_with_file_and_msgs! {goto_markers, "examples/counter.vcd", [
     Message::AddScope(ScopeRef::from_strs(&["tb"])),
     Message::CursorSet(BigInt::from(600)),
     Message::MoveMarkerToCursor(2),
-    Message::GoToMarkerPosition(2)
+    Message::GoToMarkerPosition(2, 0)
 ]}
 
 snapshot_ui_with_file_and_msgs! {
@@ -643,32 +643,32 @@ snapshot_ui_with_file_and_msgs! {command_prompt, "examples/counter.vcd", [
 
 snapshot_ui_with_file_and_msgs! {negative_cursorlocation, "examples/counter.vcd", [
     Message::AddScope(ScopeRef::from_strs(&["tb"])),
-    Message::GoToTime(Some(BigInt::from(-50))),
+    Message::GoToTime(Some(BigInt::from(-50)), 0),
     Message::CursorSet(BigInt::from(-100)),
 ]}
 
 snapshot_ui_with_file_and_msgs! {goto_start, "examples/counter.vcd", [
     Message::AddScope(ScopeRef::from_strs(&["tb"])),
-    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2},
-    Message::GoToStart
+    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2, viewport_idx: 0},
+    Message::GoToStart{viewport_idx: 0}
 ]}
 
 snapshot_ui_with_file_and_msgs! {goto_end, "examples/counter.vcd", [
     Message::AddScope(ScopeRef::from_strs(&["tb"])),
-    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2},
-    Message::GoToEnd
+    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2, viewport_idx: 0},
+    Message::GoToEnd{viewport_idx: 0}
 ]}
 
 snapshot_ui_with_file_and_msgs! {zoom_to_fit, "examples/counter.vcd", [
     Message::AddScope(ScopeRef::from_strs(&["tb"])),
-    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2},
-    Message::GoToEnd,
-    Message::ZoomToFit
+    Message::CanvasZoom {mouse_ptr_timestamp: None, delta:0.2, viewport_idx: 0},
+    Message::GoToEnd{viewport_idx: 0},
+    Message::ZoomToFit{viewport_idx: 0}
 ]}
 
 snapshot_ui_with_file_and_msgs! {zoom_to_range, "examples/counter.vcd", [
     Message::AddScope(ScopeRef::from_strs(&["tb"])),
-    Message::ZoomToRange { start: 100.0, end: 250.0 }
+    Message::ZoomToRange { start: 100.0, end: 250.0 , viewport_idx: 0}
 ]}
 
 snapshot_ui_with_file_and_msgs! {remove_item, "examples/counter.vcd", [
@@ -701,7 +701,7 @@ snapshot_ui_with_file_and_msgs! {remove_item_after_focus, "examples/counter.vcd"
 
 snapshot_ui_with_file_and_msgs! {canvas_scroll, "examples/counter.vcd", [
     Message::AddScope(ScopeRef::from_strs(&["tb"])),
-    Message::CanvasScroll { delta: Vec2 { x: 0., y: 100.} }
+    Message::CanvasScroll { delta: Vec2 { x: 0., y: 100.}, viewport_idx: 0 }
 ]}
 
 snapshot_ui_with_file_and_msgs! {move_focused_item_up, "examples/counter.vcd", [
@@ -1080,9 +1080,17 @@ snapshot_ui!(load_keep_signal_remove_unavailable_works, || {
 });
 
 snapshot_ui_with_file_and_msgs! {alignment_right_works, "examples/counter.vcd", [
-    Message::ToggleOverview,
-    Message::AddScope(ScopeRef::from_strs(&["tb"])),
-    Message::SetNameAlignRight(true)
+Message::ToggleOverview,
+Message::AddScope(ScopeRef::from_strs(&["tb"])),
+Message::SetNameAlignRight(true)
+]}
+
+snapshot_ui_with_file_and_msgs! {add_viewport_works, "examples/counter.vcd", [
+    Message::AddViewport, Message::AddViewport, Message::SetActiveScope(ScopeRef::from_strs(&["tb"])), Message::AddVariable(VariableRef::from_hierarchy_string("tb.clk")), Message::AddTimeLine(None),
+]}
+
+snapshot_ui_with_file_and_msgs! {remove_viewport_works, "examples/counter.vcd", [
+    Message::AddViewport, Message::AddViewport, Message::SetActiveScope(ScopeRef::from_strs(&["tb"])), Message::AddVariable(VariableRef::from_hierarchy_string("tb.clk")), Message::AddTimeLine(None), Message::RemoveViewport
 ]}
 
 snapshot_ui_with_file_and_msgs! {hierarchy_tree, "examples/counter.vcd", [