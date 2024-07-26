samitbasu/surfer-project-rhdl

load python translator with ui

surfer/src/menus.rs
@@ -102,7 +102,12 @@ impl State {
             b("Save state as...", Message::SaveStateFile(None)).add_closing_menu(msgs, ui);
             b("Open URL...", Message::SetUrlEntryVisible(true)).add_closing_menu(msgs, ui);
             #[cfg(not(target_arch = "wasm32"))]
-            b("Exit", Message::Exit).add_closing_menu(msgs, ui);
+            {
+                b("Open Python translator", Message::OpenPythonPluginDialog)
+                    .add_closing_menu(msgs, ui);
+                // TODO: b("Reload Python translator", Message::ReloadPythonPlugin).add_closing_menu(msgs, ui);
+                b("Exit", Message::Exit).add_closing_menu(msgs, ui);
+            }
         });
         ui.menu_button("View", |ui: &mut Ui| {
             ui.style_mut().wrap_mode = Some(TextWrapMode::Extend);
@@ -431,7 +436,7 @@ impl State {
 
                     if self
                         .blacklisted_translators
-                        .contains(&(path.root.clone(), (*translator_name).clone()))
+                        .contains(&(path.root.clone(), translator_name.to_string()))
                     {
                         false
                     } else {
@@ -449,7 +454,7 @@ impl State {
                             Err(e) => {
                                 msgs.push(Message::BlacklistTranslator(
                                     path.root.clone(),
-                                    (*translator_name).clone(),
+                                    translator_name.to_string(),
                                 ));
                                 msgs.push(Message::Error(e));
                                 false
@@ -470,7 +475,7 @@ impl State {
         }
         .and_then(|displayed_variable| displayed_variable.get_format(&displayed_field_ref.field));
 
-        let mut menu_entry = |ui: &mut Ui, name: &String| {
+        let mut menu_entry = |ui: &mut Ui, name: &str| {
             ui.radio(
                 selected_translator.map(|st| st == name).unwrap_or(false),
                 name,

surfer/src/wave_source.rs
@@ -1,4 +1,5 @@
 use std::fmt::{Display, Formatter};
+use std::future::Future;
 use std::path::PathBuf;
 use std::sync::atomic::AtomicU64;
 use std::sync::mpsc::Sender;
@@ -14,7 +15,7 @@ use color_eyre::eyre::{anyhow, WrapErr};
 use color_eyre::Result;
 use futures_util::FutureExt;
 use log::{error, info, warn};
-use rfd::AsyncFileDialog;
+use rfd::{AsyncFileDialog, FileHandle};
 use serde::{Deserialize, Serialize};
 use web_time::Instant;
 
@@ -552,52 +553,70 @@ impl State {
         )));
     }
 
-    pub fn open_file_dialog(&mut self, mode: OpenMode) {
+    fn file_dialog<F, O>(&mut self, filter: (String, Vec<String>), message: F)
+    where
+        F: FnOnce(FileHandle) -> O + Send + 'static,
+        O: Future<Output = Message> + Send + 'static,
+    {
         let sender = self.sys.channels.msg_sender.clone();
-        let keep_unavailable = self.config.behavior.keep_during_reload;
 
         perform_async_work(async move {
             if let Some(file) = AsyncFileDialog::new()
                 .set_title("Open waveform file")
-                .add_filter(
-                    "Waveform-files (*.vcd, *.fst, *.ghw)",
-                    &["vcd", "fst", "ghw"],
-                )
+                .add_filter(filter.0, &filter.1)
                 .add_filter("All files", &["*"])
                 .pick_file()
                 .await
             {
-                let keep_variables = match mode {
-                    OpenMode::Open => false,
-                    OpenMode::Switch => true,
-                };
-
-                #[cfg(not(target_arch = "wasm32"))]
-                sender
-                    .send(Message::LoadWaveformFile(
-                        Utf8PathBuf::from_path_buf(file.path().to_path_buf()).unwrap(),
-                        LoadOptions {
-                            keep_variables,
-                            keep_unavailable,
-                        },
-                    ))
-                    .unwrap();
-
-                #[cfg(target_arch = "wasm32")]
-                {
-                    let data = file.read().await;
-                    sender
-                        .send(Message::LoadWaveformFileFromData(
-                            data,
-                            LoadOptions {
-                                keep_variables,
-                                keep_unavailable,
-                            },
-                        ))
-                        .unwrap();
-                }
+                sender.send(message(file).await).unwrap();
             }
         });
+    }
+
+    pub fn open_file_dialog(&mut self, mode: OpenMode) {
+        let keep_unavailable = self.config.behavior.keep_during_reload;
+        let keep_variables = match mode {
+            OpenMode::Open => false,
+            OpenMode::Switch => true,
+        };
+        let message = move |file: FileHandle| async move {
+            if cfg!(target_arch = "wasm32") {
+                Message::LoadWaveformFileFromData(
+                    file.read().await,
+                    LoadOptions {
+                        keep_variables,
+                        keep_unavailable,
+                    },
+                )
+            } else {
+                Message::LoadWaveformFile(
+                    Utf8PathBuf::from_path_buf(file.path().to_path_buf()).unwrap(),
+                    LoadOptions {
+                        keep_variables,
+                        keep_unavailable,
+                    },
+                )
+            }
+        };
+
+        self.file_dialog(
+            (
+                "Waveform-files (*.vcd, *.fst, *.ghw)".to_string(),
+                vec!["vcd".to_string(), "fst".to_string(), "ghw".to_string()],
+            ),
+            message,
+        );
+    }
+
+    pub fn open_python_file_dialog(&mut self) {
+        self.file_dialog(
+            ("Python files (*.py)".to_string(), vec!["py".to_string()]),
+            |file| async move {
+                Message::LoadPythonTranslator(
+                    Utf8PathBuf::from_path_buf(file.path().to_path_buf()).unwrap(),
+                )
+            },
+        );
     }
 
     pub fn load_state_file(&mut self, path: Option<PathBuf>) {

binhkieudo/surfer

surfer/src/menus.rs
@@ -102,7 +102,12 @@ impl State {
             b("Save state as...", Message::SaveStateFile(None)).add_closing_menu(msgs, ui);
             b("Open URL...", Message::SetUrlEntryVisible(true)).add_closing_menu(msgs, ui);
             #[cfg(not(target_arch = "wasm32"))]
-            b("Exit", Message::Exit).add_closing_menu(msgs, ui);
+            {
+                b("Add Python translator", Message::OpenPythonPluginDialog)
+                    .add_closing_menu(msgs, ui);
+                // TODO: b("Reload Python translator", Message::ReloadPythonPlugin).add_closing_menu(msgs, ui);
+                b("Exit", Message::Exit).add_closing_menu(msgs, ui);
+            }
         });
         ui.menu_button("View", |ui: &mut Ui| {
             ui.style_mut().wrap_mode = Some(TextWrapMode::Extend);
@@ -431,7 +436,7 @@ impl State {
 
                     if self
                         .blacklisted_translators
-                        .contains(&(path.root.clone(), (*translator_name).clone()))
+                        .contains(&(path.root.clone(), translator_name.to_string()))
                     {
                         false
                     } else {
@@ -451,7 +456,7 @@ impl State {
                             Err(e) => {
                                 msgs.push(Message::BlacklistTranslator(
                                     path.root.clone(),
-                                    (*translator_name).clone(),
+                                    translator_name.to_string(),
                                 ));
                                 msgs.push(Message::Error(e));
                                 false
@@ -472,7 +477,7 @@ impl State {
         }
         .and_then(|displayed_variable| displayed_variable.get_format(&displayed_field_ref.field));
 
-        let mut menu_entry = |ui: &mut Ui, name: &String| {
+        let mut menu_entry = |ui: &mut Ui, name: &str| {
             ui.radio(
                 selected_translator.map(|st| st == name).unwrap_or(false),
                 name,

surfer/src/wave_source.rs
@@ -552,52 +552,85 @@ impl State {
         )));
     }
 
-    pub fn open_file_dialog(&mut self, mode: OpenMode) {
+    fn create_file_dialog(filter: (String, Vec<String>)) -> AsyncFileDialog {
+        AsyncFileDialog::new()
+            .set_title("Open waveform file")
+            .add_filter(filter.0, &filter.1)
+            .add_filter("All files", &["*"])
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    fn file_dialog<F>(&mut self, filter: (String, Vec<String>), message: F)
+    where
+        F: FnOnce(PathBuf) -> Message + Send + 'static,
+    {
         let sender = self.sys.channels.msg_sender.clone();
-        let keep_unavailable = self.config.behavior.keep_during_reload;
 
         perform_async_work(async move {
-            if let Some(file) = AsyncFileDialog::new()
-                .set_title("Open waveform file")
-                .add_filter(
-                    "Waveform-files (*.vcd, *.fst, *.ghw)",
-                    &["vcd", "fst", "ghw"],
-                )
-                .add_filter("All files", &["*"])
-                .pick_file()
-                .await
-            {
-                let keep_variables = match mode {
-                    OpenMode::Open => false,
-                    OpenMode::Switch => true,
-                };
-
-                #[cfg(not(target_arch = "wasm32"))]
-                sender
-                    .send(Message::LoadWaveformFile(
-                        Utf8PathBuf::from_path_buf(file.path().to_path_buf()).unwrap(),
-                        LoadOptions {
-                            keep_variables,
-                            keep_unavailable,
-                        },
-                    ))
-                    .unwrap();
-
-                #[cfg(target_arch = "wasm32")]
-                {
-                    let data = file.read().await;
-                    sender
-                        .send(Message::LoadWaveformFileFromData(
-                            data,
-                            LoadOptions {
-                                keep_variables,
-                                keep_unavailable,
-                            },
-                        ))
-                        .unwrap();
-                }
+            if let Some(file) = Self::create_file_dialog(filter).pick_file().await {
+                sender.send(message(file.path().to_path_buf())).unwrap();
             }
         });
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    fn file_dialog<F>(&mut self, filter: (String, Vec<String>), message: F)
+    where
+        F: FnOnce(Vec<u8>) -> Message + Send + 'static,
+    {
+        let sender = self.sys.channels.msg_sender.clone();
+
+        perform_async_work(async move {
+            if let Some(file) = Self::create_file_dialog(filter).pick_file().await {
+                sender.send(message(file.read().await)).unwrap();
+            }
+        });
+    }
+
+    pub fn open_file_dialog(&mut self, mode: OpenMode) {
+        let keep_unavailable = self.config.behavior.keep_during_reload;
+        let keep_variables = match mode {
+            OpenMode::Open => false,
+            OpenMode::Switch => true,
+        };
+
+        #[cfg(not(target_arch = "wasm32"))]
+        let message = move |file: PathBuf| {
+            Message::LoadWaveformFile(
+                Utf8PathBuf::from_path_buf(file).unwrap(),
+                LoadOptions {
+                    keep_variables,
+                    keep_unavailable,
+                },
+            )
+        };
+
+        #[cfg(target_arch = "wasm32")]
+        let message = move |file: Vec<u8>| {
+            Message::LoadWaveformFileFromData(
+                file,
+                LoadOptions {
+                    keep_variables,
+                    keep_unavailable,
+                },
+            )
+        };
+
+        self.file_dialog(
+            (
+                "Waveform-files (*.vcd, *.fst, *.ghw)".to_string(),
+                vec!["vcd".to_string(), "fst".to_string(), "ghw".to_string()],
+            ),
+            message,
+        );
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn open_python_file_dialog(&mut self) {
+        self.file_dialog(
+            ("Python files (*.py)".to_string(), vec!["py".to_string()]),
+            |file| Message::LoadPythonTranslator(Utf8PathBuf::from_path_buf(file).unwrap()),
+        );
     }
 
     pub fn load_state_file(&mut self, path: Option<PathBuf>) {